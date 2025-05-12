05c39c0aaa7e6563000f9703dcb5524ae22d520f
Huan610193 <huancules0510@gmail.com>
2025-05-13 00:56:34 +0700

    fix-user-query-order-and-total-inventory

--- src/main/java/com/nahuannghia/shopnhn/repository/OrderDetailRepository.java
+++ src/main/java/com/nahuannghia/shopnhn/repository/OrderDetailRepository.java
@@ -3,8 +3,11 @@ package com.nahuannghia.shopnhn.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.nahuannghia.shopnhn.Response.OrderDetailResponse;
 import com.nahuannghia.shopnhn.model.OrderDetail;
 import com.nahuannghia.shopnhn.model.OrderDetailId;
 
@@ -12,5 +15,10 @@ import com.nahuannghia.shopnhn.model.OrderDetailId;
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, OrderDetailId> {
     List<OrderDetail> findByOrderDetailIdOrderId(Integer orderId);
+    @Query("SELECT new com.nahuannghia.shopnhn.Response.OrderDetailResponse(" +
+       "od.product.productId, od.quantity, od.total_price, od.color, od.size) " +
+       "FROM OrderDetail od WHERE od.order.orderId = :orderId")
+List<OrderDetailResponse> findOrderDetailsByOrderId(@Param("orderId") Integer orderId);
+
 }
 
--- src/main/java/com/nahuannghia/shopnhn/repository/ProductRepository.java
+++ src/main/java/com/nahuannghia/shopnhn/repository/ProductRepository.java
@@ -14,19 +14,21 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     //Tìm kiếm phải viết đúng
     //@Query("SELECT p FROM Product p WHERE LOWER(p.productName) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-
-
     //TÌm kiếm có thể viết không dấu
     @Query(
-            value = "SELECT * FROM product p WHERE LOWER(p.productName) COLLATE Vietnamese_CI_AI LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-                    "LOWER(p.brand) COLLATE Vietnamese_CI_AI LIKE LOWER(CONCAT('%', :keyword, '%'))",
+            value = "SELECT * FROM product p WHERE LOWER(p.productName) COLLATE Vietnamese_CI_AI LIKE LOWER(CONCAT('%', :keyword, '%')) OR "
+            + "LOWER(p.brand) COLLATE Vietnamese_CI_AI LIKE LOWER(CONCAT('%', :keyword, '%'))",
             nativeQuery = true
-    )    List<Product> searchByNameOrBrand(String keyword);
+    )
+    List<Product> searchByNameOrBrand(String keyword);
 
     @Query("SELECT p FROM Product p ORDER BY p.createdAt DESC LIMIT 4")
     List<Product> findTop4ByOrderByCreatedDateDesc();
+
     @Query("SELECT COUNT(DISTINCT p.productId) FROM Product p LEFT JOIN ProductInventory pi ON p.productId = pi.productInventoryId.productId WHERE pi.quantity <= :count")
     long countLowStockProducts(@Param("count") int count);
+
+    @Query("SELECT sum(pi.quantity) FROM Product p LEFT JOIN ProductInventory pi ON p.productId = pi.id.productId")
+    long countTotalInventory();
+
 }
-
-

1df217167c78d8af76e24c5ac598270d3791b370
Huan610193 <huancules0510@gmail.com>
2025-06-06 11:28:58 +0700

    vvvv

--- frontend/vue-project/src/views/AboutView.vue
+++ frontend/vue-project/src/views/AboutView.vue
@@ -40,6 +40,20 @@
         </div>
       </div>
     </section>
+
+    <!-- Contact Section -->
+    <section class="section">
+      <div class="container">
+        <h2 class="section-title">Liên hệ</h2>
+        <ul class="contact-list">
+          <li class="contact-item">Địa chỉ: 54 Nguyễn Lương Bằng, Hoà Khánh Bắc, Liên Chiểu, Đà Nẵng, Việt Nam</li>
+          <li class="contact-item">Hotline: 0909 123 456</li>
+          <li class="contact-item">Email: contact@nhnshoes.vn</li>
+        </ul>
+      </div>
+    </section>
+
+
   </div>
 </template>
 
